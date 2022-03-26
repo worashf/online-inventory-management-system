e6e070f0331a9866245a3bf80d37979ff86a8247
Worash <uppert83@gmail.com>
2022-03-26 14:11:49 +0300

    company dao modified

--- src/main/java/com/inventory/deva_inventory/dao/CompanyRepository.java
+++ src/main/java/com/inventory/deva_inventory/dao/CompanyRepository.java
@@ -8,8 +8,7 @@ package com.inventory.deva_inventory.dao;
 
 import com.inventory.deva_inventory.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 import org.springframework.stereotype.Repository;
 
 /**
@@ -17,12 +16,9 @@ import org.springframework.stereotype.Repository;
  * @author best
  */
 @Repository
-public interface CompanyRepository extends JpaRepository<Company, Long>{
+public interface CompanyRepository extends JpaRepository<Company, Integer>{
     
-        @Query("Update Company c set c.companyName= :companyName, c.email = :email, c.phone1 =:phone1, c.phone2 =:phone2 "
-             + "WHERE c.companyId = :companyId")
-     public Company updateCategory(@Param("companyId") Long compId, 
-             @Param("companyName") String name, @Param("email") String email,
-              @Param("phone1") String phone1, @Param("phone2") String phone2);
+    
+       
 
 }

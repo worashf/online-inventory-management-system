cc63f86e264343653ce6e360248312d74a7f94bb
worashf <uppert83@gmail.com>
2022-05-07 22:43:08 +0300

    update supplied product model

--- src/main/java/com/inventory/deva_inventory/model/SuppliedProduct.java
+++ src/main/java/com/inventory/deva_inventory/model/SuppliedProduct.java
@@ -5,7 +5,11 @@
 package com.inventory.deva_inventory.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import java.io.Serializable;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -14,15 +18,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="supplier_product")
+
 public class SuppliedProduct  implements Serializable {
         @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name ="suppliered_product_id")
-    private Integer  suppieredProductId;
+    private Integer  suppliedProductId;
      @Column(name = "supplied_product_name")
     private String  suppliedProductName;
     @Column(name = "supplied_product_quantity")
@@ -31,10 +37,22 @@ public class SuppliedProduct  implements Serializable {
      private Double suppliedProductPrice;
      @Column(name="supplied_product_status")
      private String suppliedProductStatus;
+    
      @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name ="sale_order_id")
     private SaleOrder saleOrder;
+       @JsonIgnore
+    @OneToOne(mappedBy = "supProduct",cascade = CascadeType.ALL)
+    private Product product;
+    @JsonManagedReference(value = "product-supplied-product")
+    public Product getProduct() {
+        return product;
+    }
 
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    
     public String getSuppliedProductStatus() {
         return suppliedProductStatus;
     }
@@ -42,16 +60,17 @@ public class SuppliedProduct  implements Serializable {
     public void setSuppliedProductStatus(String suppliedProductStatus) {
         this.suppliedProductStatus = suppliedProductStatus;
     }
+
+    public Integer getSuppliedProductId() {
+        return suppliedProductId;
+    }
+
+    public void setSuppliedProductId(Integer suppliedProductId) {
+        this.suppliedProductId = suppliedProductId;
+    }
      
 
-    public Integer getSuppieredProductId() {
-        return suppieredProductId;
-    }
-
-    public void setSuppieredProductId(Integer suppieredProductId) {
-        this.suppieredProductId = suppieredProductId;
-    }
-
+  
     public String getSuppliedProductName() {
         return suppliedProductName;
     }

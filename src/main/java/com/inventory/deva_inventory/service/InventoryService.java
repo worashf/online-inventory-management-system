96c5c8e88fbbb14b12bed7585dba2a2c86239c84
worashf <uppert83@gmail.com>
2022-05-07 22:42:06 +0300

    update inventory service interface

--- src/main/java/com/inventory/deva_inventory/service/InventoryService.java
+++ src/main/java/com/inventory/deva_inventory/service/InventoryService.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.inventory.deva_inventory.service;
 
 import com.inventory.deva_inventory.model.Inventory;
 import java.util.List;
 
-/**
- *
- * @author best
- */
+
 public interface InventoryService {
-       public Inventory   saveInventory(Integer storeId, Inventory inventory);
+     public Inventory   saveInventory(Integer storeId, Inventory inventory);
      public void deleteInventory  ( Integer inventoryId);
      public Inventory  editInventory  (Integer inventoryId,Integer storeId,Inventory  inventory);
      public List<Inventory> listAllInventorys  ();
-      public List<Inventory> listAllInventorysByStoreId  (Integer storeId);
+     public List<Inventory> listAllInventorysByStoreId  (Integer storeId);
+     public Inventory findInventoryByInventoryCode(String inventoryCode);
+     public List<Inventory> findInventoryByReorderLevel(Integer reorderLevel);
+     public List<Inventory> findInventoryByAlertLevel(Integer alertLevel);
 }

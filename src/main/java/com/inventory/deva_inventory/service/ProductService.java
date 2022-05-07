fb685c3c149a45281d0c8cf1292c8c153549f663
worashf <uppert83@gmail.com>
2022-05-07 22:41:13 +0300

    update product service interface

--- src/main/java/com/inventory/deva_inventory/service/ProductService.java
+++ src/main/java/com/inventory/deva_inventory/service/ProductService.java
@@ -13,10 +13,18 @@ import java.util.List;
  * @author best
  */
 public interface ProductService {
-     public Product   saveProduct(Integer supplierId, String orderNUmber, Product  product);
+     public Product   saveProduct(Integer supplierId, Integer suppliedProductId,Integer categoryId
+             ,Integer brandId,Product  product);
      public void deleteProduct  ( Integer orderProductId);
      public Product   editProduct  (Integer productId,Product   product);
      public List<Product > listAllProduct  ();
-      public List<Product > listAllProductById  (Integer orderId);
+     public List<Product > listAllProductById  (Integer orderId);
+     public List<Product > listAllProductByCategoryId (Integer categoryId);
+     public List<Product > listAllProductByBrandId  (Integer brandId);
+     public  List<Product> listAllProductByProductStockStatus(String stockStatus);
+    public Product getProductByProductNumber(String productNumber);
+    public Product addProductToInventory(Integer productId, String inventoryCode);
+    public  List<Product> listProductByInventory(Integer inventoryId);
+    
      
 }

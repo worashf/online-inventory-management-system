e57c1d3f07d656d685d71bdbf4ca73df01f3e319
worashf <uppert83@gmail.com>
2022-05-07 22:52:52 +0300

    update product end points

--- src/main/java/com/inventory/deva_inventory/controller/ProductController.java
+++ src/main/java/com/inventory/deva_inventory/controller/ProductController.java
@@ -32,10 +32,10 @@ public class ProductController {
     @Autowired
     private ProductService prodService;
         
-         @PostMapping("/products/{supplierId}/{orderNumber}")
-   public ResponseEntity<Product> saveProduct(@PathVariable Integer supplierId,@PathVariable String orderNumber ,@RequestBody Product ProData){ 
-                Product product = prodService.saveProduct(supplierId,orderNumber, ProData);
-               System.out.println(supplierId+ " " + orderNumber);
+         @PostMapping("/products/{supplierId}/{suppliedProductId}/{categoryId}/{brandId}")
+   public ResponseEntity<Product> saveProduct(@PathVariable Integer supplierId,@PathVariable Integer suppliedProductId,
+           @PathVariable Integer categoryId, @PathVariable Integer brandId,@RequestBody Product ProData){ 
+                Product product = prodService.saveProduct(supplierId, suppliedProductId, categoryId, brandId, ProData);
                 return  ResponseEntity.ok().body(product);
     }
     @PutMapping("/products/{productId}")
@@ -51,12 +51,44 @@ public class ProductController {
          List<Product> listPro = prodService.listAllProduct();
         return ResponseEntity.ok().body(listPro);
     }
+        @GetMapping("/products/un-stocked")
+    public ResponseEntity<List<Product>> getAllUnStockedProducts(){
+       
+         List<Product> listPro = prodService.listAllProductByProductStockStatus("un-stocked");
+        return ResponseEntity.ok().body(listPro);
+    }
+        @GetMapping("/products/stocked")
+    public ResponseEntity<List<Product>> getAllStockedProducts(){
+       
+         List<Product> listPro = prodService.listAllProductByProductStockStatus("stocked");
+        return ResponseEntity.ok().body(listPro);
+    }
        @GetMapping("/products/{orderId}")
-    public ResponseEntity<List<Product>> getAllProductsByOrderId(@PathVariable Integer orderId){
+    public ResponseEntity<List<Product>>  getAllProductsByCategory(@PathVariable Integer orderId){
        
          List<Product> listPro = prodService.listAllProductById(orderId);
         return ResponseEntity.ok().body(listPro);
     }
+        @GetMapping("/products/category/{categoryId}")
+    public ResponseEntity<List<Product>> getAllProductsByOrderId(@PathVariable Integer categoryId){
+       
+         List<Product> listPro = prodService.listAllProductByCategoryId(categoryId);
+        return ResponseEntity.ok().body(listPro);
+    }
+        @GetMapping("/products/brand/{brandId}")
+    public ResponseEntity<List<Product>> getAllProductsByBrandId(@PathVariable Integer brandId){
+       
+         List<Product> listPro = prodService.listAllProductByBrandId(brandId);
+        return ResponseEntity.ok().body(listPro);
+    }
+    
+     @GetMapping("/products/product-number/{productNumber}")
+    public ResponseEntity<Product> getAllProductsByProductNumber(@PathVariable String productNumber){
+       
+         Product pro = prodService.getProductByProductNumber(productNumber);
+        return ResponseEntity.ok().body(pro);
+    }
+    
     @DeleteMapping("/products/{productId}")
     public ResponseEntity<Map<String,Boolean>> deleteProduct(@PathVariable Integer productId){
            prodService.deleteProduct(productId);
@@ -64,6 +96,18 @@ public class ProductController {
          response.put("deleted", Boolean.TRUE);
          return ResponseEntity.ok(response);
     }
+    
+       @PutMapping("/products/add-product-inventory/{productId}/{inventoryCode}")
+    public ResponseEntity<Product>  addProductToInventory(@PathVariable Integer productId,
+            @PathVariable String inventoryCode){
+           System.out.println(productId + "this " + inventoryCode);
+         Product pro = prodService.addProductToInventory(productId, inventoryCode);
+        return ResponseEntity.ok().body(pro);
+    }
+    
+    
+    
+    
 
 
     

4fbda67735cbccd3e818e3a9cca4bc35320a3941
worashf <uppert83@gmail.com>
2022-05-07 22:50:37 +0300

    update product data access

--- src/main/java/com/inventory/deva_inventory/dao/ProductRepository.java
+++ src/main/java/com/inventory/deva_inventory/dao/ProductRepository.java
@@ -19,7 +19,17 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ProductRepository  extends  JpaRepository<Product, Integer>{
-        @Query("SELECT p FROM Product p  JOIN  p.order o  WHERE o.orderId =:orderId")
+    @Query("SELECT p FROM Product p  JOIN  p.order o  WHERE o.orderId =:orderId")
      List<Product>  getAllProductByOrderId(@Param (value ="orderId") Integer orderId);
-      
+     @Query("SELECT p FROM Product p  JOIN  p.category c  WHERE c.categoryId =:categoryId")
+    List<Product> getAllProductByCategory(@Param (value = "categoryId") Integer brandId);
+    @Query("SELECT p FROM Product p  JOIN  p.brand b  WHERE b.brandId=:brandId")
+    List<Product> getAllProductByBrand(@Param (value = "brandId") Integer brandId);
+    @Query("SELECT p FROM Product p  JOIN  p.inventory inv  WHERE inv.inventoryId=:inventoryId")
+    List<Product> getAllProductByInventory(@Param (value = "inventoryId") Integer inventoryId);
+    @Query("SELECT p FROM Product p   WHERE p.productNumber=:productNumber ")
+    Product getProductbyProductNumner(@Param (value = "productNumber")String productNumber);
+   @Query("SELECT p FROM Product p    WHERE p.stockStatus=:stockStatus")
+    List<Product> getAllProductByStockStatus(@Param (value = "stockStatus") String stockStatus);
+   
 }
